32e3f734196780b283b8ea0334e889cdbb448cbd
shane <moloney.shane@yahoo.com>
2020-01-28 07:59:11 +0000

    read in files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 extern crate regex;
 extern crate clap;
 
+use std::fs::File;
+use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
 use clap::{App,Arg};
 use regex::Regex;
 
-fn main() {
-    let haystack = "Every face, every shop,
-    bedroom window, public-house, and
-dark square is a picture
-feverishly turned--in search of what?
-It is the same with books.
-What do we seek
-through millions of pages?";
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex){
+    for line_ in reader.lines(){
+        let line = line_.unwrap();
+        match re.find(&line){
+            Some(_)=> println!("{}, line"),
+            None => (),
+        }
+    }
+}
 
+fn main() {
     let args = App::new("grep-lite")
         .version("0.1")
         .about("searches for patterns")
@@ -20,60 +26,24 @@ through millions of pages?";
             .help("the pattern to search for")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(true))
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    for line in haystack.lines() {
-        match re.find(line) {
-            Some(_) => println!("{:?}", line),
-            None => ()
-        }
+    let input = args.value_of("input").unwrap_or("-");
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
-
-
-/*
-
-    let context_lines = 2;
-    let needle = "books";
-    let mut tags: Vec<usize> = Vec::new();
-    let mut ctx: Vec<Vec<(usize, String)>> = Vec::new();
-
-    //PASS 1
-    for (i, line) in haystack.lines().enumerate() {
-        if line.contains(needle) {
-            tags.push(i);
-
-            let v = Vec::with_capacity(2*context_lines + 1);
-            ctx.push(v);
-        }
-    }
-
-    if tags.len() == 0 {
-        return;
-    }
-
-    //PASS 2
-    for (i, line) in haystack.lines().enumerate() {
-        for (j, tag) in tags.iter().enumerate() {
-            let lower_bound = tag.saturating_sub(context_lines);
-            let upper_bound = tag + context_lines;
-
-            if (i >= lower_bound) && (i <= upper_bound){
-                let line_as_string = String::from(line);
-                let local_ctx = (i, line_as_string);
-                ctx[j].push(local_ctx);
-            }
-        }
-    }
-    //OUTPUT
-    for local_ctx in ctx.iter(){
-        for &(i, ref line) in local_ctx.iter(){
-            let line_num = i +1;
-            println!("{}: {}", line_num, line);
-        }
-    }
-    */
-    }
-
+}
